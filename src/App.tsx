2dab814db8f16d21f1761a62a50f409a73df6799
SplitAI <splitai@local>
2026-06-26 08:58:40 -0300

    feat(app): wire friends and expense routes

--- src/App.tsx
+++ src/App.tsx
@@ -4,6 +4,10 @@ import { getDb } from './db/db';
 import { Layout } from './components/Layout';
 import { SetupPage } from './pages/SetupPage';
 import { NotFoundPage } from './pages/NotFoundPage';
+import { FriendsPage } from './pages/FriendsPage';
+import { FriendDetailPage } from './pages/FriendDetailPage';
+import { ExpenseFormPage } from './pages/ExpenseFormPage';
+import { ExpenseDetailPage } from './pages/ExpenseDetailPage';
 
 function ProfileGate() {
   const hasProfile = useLiveQuery(async () => {
@@ -22,13 +26,18 @@ export default function App() {
         <Route path="/setup" element={<SetupPage />} />
         <Route element={<ProfileGate />}>
           <Route element={<Layout />}>
-            <Route path="/" element={<div className="p-4">Home placeholder</div>} />
-            <Route path="/groups" element={<div className="p-4">Groups placeholder</div>} />
-            <Route path="/friends" element={<div className="p-4">Friends placeholder</div>} />
-            <Route path="/dashboard" element={<div className="p-4">Dashboard placeholder</div>} />
-            <Route path="/search" element={<div className="p-4">Search placeholder</div>} />
-            <Route path="/recurring" element={<div className="p-4">Recurring placeholder</div>} />
-            <Route path="/more" element={<div className="p-4">More placeholder</div>} />
+            <Route path="/" element={<div className="p-4">Home placeholder (Task 23)</div>} />
+            <Route path="/groups" element={<div className="p-4">Groups placeholder (Task 24)</div>} />
+            <Route path="/groups/:id" element={<div className="p-4">Group detail placeholder (Task 24)</div>} />
+            <Route path="/friends" element={<FriendsPage />} />
+            <Route path="/friends/:id" element={<FriendDetailPage />} />
+            <Route path="/expenses/new" element={<ExpenseFormPage />} />
+            <Route path="/expenses/:id" element={<ExpenseDetailPage />} />
+            <Route path="/expenses/:id/edit" element={<ExpenseFormPage />} />
+            <Route path="/dashboard" element={<div className="p-4">Dashboard placeholder (Task 23)</div>} />
+            <Route path="/search" element={<div className="p-4">Search placeholder (Task 26)</div>} />
+            <Route path="/recurring" element={<div className="p-4">Recurring placeholder (Task 27)</div>} />
+            <Route path="/more" element={<div className="p-4">More placeholder (Task 28)</div>} />
           </Route>
         </Route>
         <Route path="*" element={<NotFoundPage />} />
